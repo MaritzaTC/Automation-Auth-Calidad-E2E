032c40de0916ec4ebfbd8771d5ee4b3e7796f054
MaritzaTC <maritza.tabarezc@udea.edu.co>
2024-11-18 18:00:15 -0500

    feat: Initialize project stricture with Gradle setup and basic configurations
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: JuanVasquezO <juan.vasquez21@udea.edu.co>

---
+++ src/main/java/com/udea/automationauthcalidade2e/AutomationAuthCalidadE2eApplication.java
@@ -0,0 +1,13 @@
+package com.udea.automationauthcalidade2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationAuthCalidadE2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationAuthCalidadE2eApplication.class, args);
+    }
+
+}
