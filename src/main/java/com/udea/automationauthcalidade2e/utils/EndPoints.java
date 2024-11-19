396cc2d456b1ba2b2dd4ab98d81c2b43768e3016
MaritzaTC <maritza.tabarezc@udea.edu.co>
2024-11-18 21:14:30 -0500

    feat: Add EndPoints utility class for login and signup URLs
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: JuanVasquezO <juan.vasquez21@udea.edu.co>

---
+++ src/main/java/com/udea/automationauthcalidade2e/utils/EndPoints.java
@@ -0,0 +1,11 @@
+package com.udea.automationauthcalidade2e.utils;
+
+public class EndPoints {
+
+    private EndPoints() {
+
+    }
+    public static final String URL_LOGIN = "login";
+
+    public static final String URL_SIGNUP = "signup";
+}
